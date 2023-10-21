05612643e99b6d155cb49e2b2c6939b632c7b254
eniovi <enio.vi.santos@gmail.com>
2023-10-21 15:50:19 -0300

    added annotations in entity class

--- src/main/kotlin/com/eniovi/credit/application/system/entity/Credit.kt
+++ src/main/kotlin/com/eniovi/credit/application/system/entity/Credit.kt
@@ -1,16 +1,32 @@
 package com.eniovi.credit.application.system.entity
 
 import com.eniovi.credit.application.system.enumx.Status
+import jakarta.persistence.*
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.*
 
+@Entity
 data class Credit(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = false, unique = true)
     val creditCode: UUID = UUID.randomUUID(),
+
+    @Column(nullable = false)
     val creditValue: BigDecimal = BigDecimal.ZERO,
+
+    @Column(nullable = false)
     val dayFirstInstallment: LocalDate,
+
+    @Column(nullable = false)
     val numberOfInstallments: Int = 0,
+
+    @Enumerated
     val status: Status = Status.IN_PROGRESS,
-    val customer: Customer? = null,
-    val id: Long? = null
+
+    @ManyToOne
+    val customer: Customer? = null
 )
