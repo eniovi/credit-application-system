4bcfe754a20db8aa09de40bb6544fe639ed3b6ff
eniovi <enio.vi.santos@gmail.com>
2023-10-25 18:10:33 -0300

    refact method update

--- src/main/kotlin/com/eniovi/credit/application/system/service/ICustomerService.kt
+++ src/main/kotlin/com/eniovi/credit/application/system/service/ICustomerService.kt
@@ -1,10 +1,11 @@
 package com.eniovi.credit.application.system.service
 
+import com.eniovi.credit.application.system.dto.CustomerUpdateDto
 import com.eniovi.credit.application.system.entity.Customer
 
 interface ICustomerService {
     fun save(customer: Customer): Customer
     fun findById(customerId: Long): Customer
     fun delete(customerId: Long)
-    fun update(customer: Customer): Customer
+    fun update(id: Long, customerUpdate: CustomerUpdateDto): Customer
 }
