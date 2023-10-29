e4d6102826e3df95a7c7d5b2d2bf8cae6f41b772
eniovi <enio.vi.santos@gmail.com>
2023-10-29 16:22:09 -0300

    added validations in DTOs

--- src/main/kotlin/com/eniovi/credit/application/system/dto/CreditDto.kt
+++ src/main/kotlin/com/eniovi/credit/application/system/dto/CreditDto.kt
@@ -2,13 +2,20 @@ package com.eniovi.credit.application.system.dto
 
 import com.eniovi.credit.application.system.entity.Credit
 import com.eniovi.credit.application.system.entity.Customer
+import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class CreditDto(
+    @field:NotNull(message = "Credit Value is required")
     val creditValue: BigDecimal,
+    @field:Future(message = "Day first installment must be after current date")
     val dayFirstInstallment: LocalDate,
+    @field:Positive(message = "Number of installments must be bigger than zero")
     val numberOfInstallments: Int,
+    @field:NotNull(message = "Customer Id is required")
     var customerId: Long
 ) {
 
