a76ee03734d0512d0ed11e933e387bacfe9ffe6d
eniovi <enio.vi.santos@gmail.com>
2023-10-29 16:25:30 -0300

    created ExceptionDetails

---
+++ src/main/kotlin/com/eniovi/credit/application/system/exception/ExceptionDetails.kt
@@ -0,0 +1,10 @@
+package com.eniovi.credit.application.system.exception
+
+import java.time.LocalDateTime
+
+data class ExceptionDetails(
+    val timestamp: LocalDateTime,
+    val status: Int,
+    val exception: String,
+    val details: MutableMap<String, String?>
+)
