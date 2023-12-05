4cac66bcef30245fecef244b6ff2a6dbc524373f
eniovi <enio.vi.santos@gmail.com>
2023-12-05 10:43:33 -0300

    created test for CustomerController

---
+++ src/test/kotlin/com/eniovi/credit/application/system/controller/CustomerControllerTest.kt
@@ -0,0 +1,120 @@
+package com.eniovi.credit.application.system.controller
+
+import com.eniovi.credit.application.system.dto.CustomerDto
+import com.eniovi.credit.application.system.repository.CustomerRepository
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.math.BigDecimal
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@ContextConfiguration
+class CustomerControllerTest {
+    @Autowired
+    private lateinit var customerRepository: CustomerRepository
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    companion object {
+        const val URL: String = "/api/customers"
+    }
+
+    @BeforeEach
+    fun setup() = customerRepository.deleteAll()
+
+    @Test
+    fun `should create a customer and return 201 status`() {
+        val customerDto: CustomerDto = builderCustomerDto()
+        val valueAsString = objectMapper.writeValueAsString(customerDto)
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+        )
+            .andExpect(MockMvcResultMatchers.status().isCreated)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Enio"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Santos"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value("345.091.580-09"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("enio@teste.com.br"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("12345"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Rua do Enio"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+    }
+
+    @Test
+    fun `should not save a customer with same CPF or email and return 409 status`() {
+        customerRepository.save(builderCustomerDto().toCustomer())
+
+        val customerDto: CustomerDto = builderCustomerDto()
+        val valueAsString = objectMapper.writeValueAsString(customerDto)
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+        )
+            .andExpect(MockMvcResultMatchers.status().isConflict)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(409))
+            .andExpect(
+                MockMvcResultMatchers.jsonPath("$.exception")
+                    .value("org.springframework.dao.DataIntegrityViolationException")
+            )
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+    }
+
+    @Test
+    fun `should not save a customer with firstName empty and return 400 status`() {
+        val customerDto: CustomerDto = builderCustomerDto(firstName = "")
+        val valueAsString = objectMapper.writeValueAsString(customerDto)
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+        )
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(400))
+            .andExpect(
+                MockMvcResultMatchers.jsonPath("$.exception")
+                    .value("org.springframework.web.bind.MethodArgumentNotValidException")
+            )
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+    }
+
+    private fun builderCustomerDto(
+        firstName: String = "Enio",
+        lastName: String = "Santos",
+        cpf: String = "345.091.580-09",
+        email: String = "enio@teste.com.br",
+        income: BigDecimal = BigDecimal.valueOf(1000.00),
+        password: String = "123456",
+        street: String = "Rua do Enio",
+        zipCode: String = "12345"
+    ) = CustomerDto(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        income = income,
+        password = password,
+        zipCode = zipCode,
+        street = street
+    )
+
+}
